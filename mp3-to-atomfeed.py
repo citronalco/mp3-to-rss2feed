4a5202f82209de400bbd16ae1bedf30431b550e4
Bernhard Geier <2651739+citronalco@users.noreply.github.com>
2020-09-01 23:55:42 +0200

    WIP

--- mp3-to-atomfeed.py
+++ mp3-to-atomfeed.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 # Todo:
-# - testen
+# - testen (fm4 + zündfunk)
 # - Mehr id3-Tags verwenden
-# - nicht auf TLEN verlassen
 # - ID3-Chapters
+# - validieren (https://castfeedvalidator.com/?url=https://www.geierb.de/test/test.xml)
+# - doku: https://help.apple.com/itc/podcasts_connect/#/itcb54353390
 
 import sys
 from os import stat, listdir, path
@@ -14,6 +15,7 @@ from datetime import datetime, timezone
 import urllib.parse
 from hashlib import md5
 from mutagen.id3 import ID3,ID3NoHeaderError,TRSN,TPE1,TALB,TRCK,TIT2,COMM,TYER,TDAT,TIME,TLEN,TDRL,CTOC,CHAP,CTOCFlags
+import mutagen
 
 if len(sys.argv) < 4 or len(sys.argv) > 5:
     print("Usage:", file=sys.stderr)
@@ -40,21 +42,25 @@ NOW=datetime.now(timezone.utc).strftime("%a, %d %b %Y %T %z")
 root = Element('rss', attrib={
     'version': '2.0',
     'xmlns:itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
-    'xmlns:atom': 'http://www.w3.org/2005/Atom'
+    'xmlns:atom': 'http://www.w3.org/2005/Atom',
+    'xmlns:content': 'http://www.purl.org/rss/1.0/modules/content/',
 })
 
 channel = SubElement(root,"channel")
-channel.append(Element('link', text=LINK))
-channel.append(Element('atom:link', attrib={
+
+SubElement(channel,'link').text=LINK
+SubElement(channel,'atom:link', attrib={
     'href': LINK+'/podcast.xml',
     'rel': 'self',
     'type': 'application/rss+xml'
-}))
-channel.append(Element('language', text='de'))
-channel.append(Element('pubDate', text=NOW))
-channel.append(Element('title', text=FEEDTITLE))
-channel.append(Element('description', text=FEEDTITLE))
-channel.append(Element('itunes:summary', text=FEEDTITLE))
+})
+SubElement(channel,"language").text="de"
+
+#channel.append(Element('language','de'))
+SubElement(channel,'pubDate').text=NOW
+SubElement(channel,'title').text=FEEDTITLE
+SubElement(channel,'description').text=FEEDTITLE
+SubElement(channel,'itunes:summary').text=FEEDTITLE
 if IMAGE is not None:
     image = SubElement(channel,"image")
     image.append(Element('url', text=IMAGE))
@@ -88,50 +94,46 @@ for filename in listdir(DIR):
     tags = {
         'TRSN': None, 'TPE1': None, 'TALB': None, 'TRCK': None,
         'TIT2': None, 'COMM': None, 'TYER': None, 'TDAT': None,
-        'TIME': None, 'TLEN': None 
+        'TIME': None, 'TLEN': None
     }
 
-
-    # FIXME
-    print(id3['COMM::XXX'])
-    sys.exit(0)
-
-
     for t in tags:
-        if t in id3.keys():
-           tags[t] = id3[t].text[0]
-#           tags[t] = id3[t][0]
-
-
-    print(tags)
-    sys.exit(0)
+       try:
+           tags[t] = id3.getall(t)[0].text[0]
+       except:
+           tags[t] = None
 
     fileinfo['desc']=tags['COMM']
     fileinfo['title']=tags['TIT2']
-    fileinfo['duration']=tags['TLEN']
-#    fileinfo['itunes-duration']=time.strftime("%H:%M:%S",time.gmtime(int(tags['TLEN']/1000)))
+
+    if tags['TLEN'] is not None:
+        fileinfo['duration']=tags['TLEN']
+    else:
+        fileinfo['duration']=mutagen.File(filepath).info.length * 1000
+
+    fileinfo['itunes-duration']=time.strftime("%H:%M:%S",time.gmtime(int(int(fileinfo['duration'])/1000)))
 
     mediafiles.append(fileinfo)
 
-    print(fileinfo)
-    sys.exit(0)
+#    print(fileinfo)
+#    sys.exit(0)
 
-print(mediafiles)
-sys.exit(0)
+#print(mediafiles)
+#sys.exit(0)
 
 for mediafile in sorted(mediafiles, key=lambda x: x['mtime'], reverse=True):
     item = SubElement(channel, "item")
-    item.append(Element('title', text=mediafile['title']))
-    item.append(Element('description', text=mediafile['desc']))
-    item.append(Element('itunes:summary', text=mediafile['desc']))
-    item.append(Element('enclosure', attrib={
+    SubElement(item,'title').text=mediafile['title']
+    SubElement(item,'description').text=mediafile['desc']
+    SubElement(item,'itunes:summary').text=mediafile['desc']
+    SubElement(item,'enclosure', attrib={
         'url': mediafile['url'],
         'type': "audio/mpeg",
-        'length': mediafile['size']
-    }))
-    item.append(Element('guid', text=mediafile['guid']))
-    item.append(Element('pubDate', text=mediafile['mdatetime']))
-#    item.append(Element('itunes:duration', text=mediainfo['itunes-duration']))
+        'length': str(mediafile['size'])
+    })
+    SubElement(item,'guid').text=mediafile['guid']
+    SubElement(item,'pubDate').text=mediafile['mdatetime']
+    SubElement(item,'itunes:duration').text=mediafile['itunes-duration']
 
 print(tostring(root, encoding='utf8', method='xml').decode())
 #ElementTree(root).write("test.xml",encoding="utf-8", xml_declaration=True)
