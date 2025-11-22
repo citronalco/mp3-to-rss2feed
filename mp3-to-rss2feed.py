a0dde3c96c5cbc4fa4e09f3d6a1886c52cea6e73
Bernhard Geier <2651739+citronalco@users.noreply.github.com>
2025-11-22 21:03:03 +0100

    wip

--- mp3-to-rss2feed.py
+++ mp3-to-rss2feed.py
@@ -31,138 +31,134 @@ class Fileinfo:
     self.guid = md5(filename.encode()).hexdigest()
     self.mimetype = None
 
-class FileinfoOpus(Fileinfo):
-  def __init__(self, filepath):
-    super().__init__(filepath)
-    self.mimetype = 'audio/opus'
-    try:
-      meta = OggOpus(filepath).tags
-
-      # simple best effort mapping
-      tag_mappings = {
-        'title': ['title'],
-        'author': ['artist', 'performer'],
-        'desc': ['comment', 'desc', 'lyrics']
-      }
-      for attr in tag_mappings:
-        for tag in tag_mappings[attr]:
-          try:
-            setattr(self, attr, " - ".join(meta[tag]))
-            break
-          except KeyError:
-            pass
-
-      # chapters
-      for key in meta.keys():
-        chapterMatch = re.match('^(chapter\d+)$', key)
-        if chapterMatch:
-          chapterKey = chapterMatch.group(1)
-          chapter = {}
-          startMatch = re.match('^(\d{1,2}):(\d{1,2}):(\d{1,2})', meta[chapterKey][0])
-          if startMatch:
-            chapter = {}
-            chapter['start'] = int(timedelta(hours=int(startMatch.group(1)), minutes=int(startMatch.group(2)), seconds = int(startMatch.group(3))).total_seconds())
-            try:
-              chapter['title'] = meta[chapterKey + "name"][0]
-            except KeyError:
-              chapter['title'] = chapterKey
-            self.chapters.append(chapter)
-
-      # pubdate
+    def update_info_opus():
+      self.mimetype = 'audio/opus'
       try:
-        self.pubdate = parser.isoparse(meta['date'][0])
-      except (KeyError, ValueError):
+        meta = OggOpus(filepath).tags
+
+        # simple best effort mapping
+        tag_mappings = {
+          'title': ['title'],
+          'author': ['artist', 'performer'],
+          'desc': ['comment', 'desc', 'lyrics']
+        }
+        for attr in tag_mappings:
+          for tag in tag_mappings[attr]:
+            try:
+              setattr(self, attr, " - ".join(meta[tag]))
+              break
+            except KeyError:
+              pass
+
+        # chapters
+        for key in meta.keys():
+          chapterMatch = re.match('^(chapter\d+)$', key)
+          if chapterMatch:
+            chapterKey = chapterMatch.group(1)
+            chapter = {}
+            startMatch = re.match('^(\d{1,2}):(\d{1,2}):(\d{1,2})', meta[chapterKey][0])
+            if startMatch:
+              chapter = {}
+              chapter['start'] = int(timedelta(hours=int(startMatch.group(1)), minutes=int(startMatch.group(2)), seconds = int(startMatch.group(3))).total_seconds())
+              try:
+                chapter['title'] = meta[chapterKey + "name"][0]
+              except KeyError:
+               chapter['title'] = chapterKey
+              self.chapters.append(chapter)
+
+        # pubdate
+        try:
+          self.pubdate = parser.isoparse(meta['date'][0])
+        except (KeyError, ValueError):
+          pass
+
+      except OggOpusHeaderError:
+        print("WARNING: %s has no OggOpus header, skipping" % filename, file=sys.stderr)
         pass
 
-    except OggOpusHeaderError:
-      print("WARNING: %s has no OggOpus header, skipping" % filename, file=sys.stderr)
-      pass
+    def update_info_mp3():
+      self.mimetype = 'audio/mpeg'
+      try:
+        ### read ID3 tag
+        id3 = ID3(filepath)
 
-class FileinfoMp3(Fileinfo):
-  def __init__(self, filepath):
-    super().__init__(filepath)
-    self.mimetype = 'audio/mpeg'
-    try:
-      ### read ID3 tag
-      id3 = ID3(filepath)
+        tagFields = {
+          'TIT2': None,
+          'TPE1': None, 'TPE2': None,
+          'COMM': None,
+          'TYER': None, 'TDAT': None, 'TIME': None, 
+          'TDRC': None, 'TDRL': None, 'TDOR': None,
+          'TLEN': None,
+          'WOAS': None, 'WORS': None,
+        }
 
-      tagFields = {
-        'TIT2': None,
-        'TPE1': None, 'TPE2': None,
-        'COMM': None,
-        'TYER': None, 'TDAT': None, 'TIME': None, 
-        'TDRC': None, 'TDRL': None, 'TDOR': None,
-        'TLEN': None,
-        'WOAS': None, 'WORS': None,
-      }
-
-      for field in tagFields:
-        try:
-          tagFields[field] = id3.getall(field)[0].text[0]
-        except AttributeError:
-          tagFields[field] = str(id3.getall(field)[0])
-        except:
-          tagFields[field] = None
+        for field in tagFields:
+          try:
+            tagFields[field] = id3.getall(field)[0].text[0]
+          except AttributeError:
+            tagFields[field] = str(id3.getall(field)[0])
+          except:
+            tagFields[field] = None
 
 
-      ### update fileinfo with info from ID3 frames
-      # title
-      if tagFields['TIT2']:
-        self.title = tagFields['TIT2']
+        ### update fileinfo with info from ID3 frames
+        # title
+        if tagFields['TIT2']:
+          self.title = tagFields['TIT2']
 
-      # artist/author
-      if tagFields['TPE1']:
-        self.author = tagFields['TPE1']
-      elif tagFields['TPE2']:
-        self.author = tagFields['TPE2']
+        # artist/author
+        if tagFields['TPE1']:
+          self.author = tagFields['TPE1']
+        elif tagFields['TPE2']:
+          self.author = tagFields['TPE2']
 
-      # desc
-      if tagFields['COMM']:
-        self.desc = tagFields['COMM']
+        # desc
+        if tagFields['COMM']:
+          self.desc = tagFields['COMM']
 
-      # duration
-      if tagFields['TLEN']:
-        self.duration = round(int(tagFields['TLEN'])/1000)
+        # duration
+        if tagFields['TLEN']:
+          self.duration = round(int(tagFields['TLEN'])/1000)
 
-      # chapters
-      for cFrame in id3.getall('CHAP'):
-        chapter = {}
-        try:
-          chapter['title'] = cFrame.sub_frames.getall('TIT2')[0].text[0]
-        except IndexError:
-          chapter['title'] = cFrame.element_id
-          chapter['start'] = round(int(int(cFrame.start_time)/1000))	# start time in seconds
-          self.chapters.append(chapter)
+        # chapters
+        for cFrame in id3.getall('CHAP'):
+          chapter = {}
+          try:
+            chapter['title'] = cFrame.sub_frames.getall('TIT2')[0].text[0]
+          except IndexError:
+            chapter['title'] = cFrame.element_id
+            chapter['start'] = round(int(int(cFrame.start_time)/1000))	# start time in seconds
+            self.chapters.append(chapter)
 
-      # pubdate (ID3v2.3: TYER,TDAT,TIME ID3v2.4: TRDC/TDRL - start with 1.1.1970 and modify it with the availble frames)
-      dt = datetime.fromtimestamp(0,timezone.utc).astimezone()
-      if tagFields['TYER']:
-        dt = dt.replace(year=tagFields['TYER'])
-      if tagFields['TDAT']:
-        dt = dt.replace(day=tagFields['TDAT'][0:2],month=tagFields['TDAT'][2:4])
-      if tagFields['TIME']:
-        dt = dt.replace(hour=tagFields['TIME'][0:2],minute=tagFields['TIME'][2:4])
+        # pubdate (ID3v2.3: TYER,TDAT,TIME ID3v2.4: TRDC/TDRL - start with 1.1.1970 and modify it with the availble frames)
+        dt = datetime.fromtimestamp(0,timezone.utc).astimezone()
+        if tagFields['TYER']:
+          dt = dt.replace(year=tagFields['TYER'])
+        if tagFields['TDAT']:
+          dt = dt.replace(day=tagFields['TDAT'][0:2],month=tagFields['TDAT'][2:4])
+        if tagFields['TIME']:
+          dt = dt.replace(hour=tagFields['TIME'][0:2],minute=tagFields['TIME'][2:4])
 
-      for x in 'TDRC','TDRL','TDOR':
-        try:
-          if tagFields[x]:
-            dt=dt.replace(year=tagFields[x].year, month=tagFields[x].month, day=tagFields[x].day, hour=tagFields[x].hour, minute=tagFields[x].minute, second=tagFields[x].second)
-            break;
-        except:
-          continue
+        for x in 'TDRC','TDRL','TDOR':
+          try:
+            if tagFields[x]:
+              dt=dt.replace(year=tagFields[x].year, month=tagFields[x].month, day=tagFields[x].day, hour=tagFields[x].hour, minute=tagFields[x].minute, second=tagFields[x].second)
+              break;
+          except:
+            continue
 
-      if dt != datetime.fromtimestamp(0,timezone.utc).astimezone():
-        self.pubdate = dt
+        if dt != datetime.fromtimestamp(0,timezone.utc).astimezone():
+          self.pubdate = dt
 
-      # link
-      if tagFields['WOAS']:
-        self.link = tagFields['WOAS']
-      elif tagFields['WORS']:
-        self.link = tagFields['WORS']
+        # link
+        if tagFields['WOAS']:
+          self.link = tagFields['WOAS']
+        elif tagFields['WORS']:
+          self.link = tagFields['WORS']
 
-    except ID3NoHeaderError:
-      #print("WARNING: %s has no ID3 tag, skipping" % filename, file=sys.stderr)
-      pass
+      except ID3NoHeaderError:
+        #print("WARNING: %s has no ID3 tag, skipping" % filename, file=sys.stderr)
+        pass
 
 
 
